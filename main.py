06c43d804d1aad67cc93336584a894f689cec271
AyazN <92933619+AyazN@users.noreply.github.com>
2021-11-02 18:47:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtCore import QPoint, QLine
-from PyQt5.QtGui import QPainter, QPolygon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
+from PyQt5.QtGui import QPainter, QPolygon, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QColorDialog
 
 
 class Progr(QMainWindow):
@@ -10,6 +10,8 @@ class Progr(QMainWindow):
         super().__init__()
         uic.loadUi('Paint.ui', self)
         self.points = []
+        self.colorbrush = QColor(240, 240, 240)
+        self.colorpen = QColor(0, 0, 0)
         self.takemousepos = False
         self.polygondraw = False
         self.buttonpolygonactiv = False
@@ -24,6 +26,8 @@ class Progr(QMainWindow):
         self.btnrectangle.clicked.connect(self.buttonrectangle)
         self.btnline.clicked.connect(self.buttonline)
         self.btncurve.clicked.connect(self.buttoncurve)
+        self.btnclrbrush.clicked.connect(self.buttonclrbrush)
+        self.btnclrpen.clicked.connect(self.buttonclrpen)
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -68,6 +72,16 @@ class Progr(QMainWindow):
             self.startpostaken = False
             self.takemousepos = False
 
+    def buttonclrbrush(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.colorbrush = QColor(color.getRgb()[0], color.getRgb()[1], color.getRgb()[2])
+
+    def buttonclrpen(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.colorpen = QColor(color.getRgb()[0], color.getRgb()[1], color.getRgb()[2])
+
     def buttonpolygon(self):
         self.sides, ok_pressed = QInputDialog.getInt(
             self, "Количество углов", "Введите количество вершин",
@@ -94,6 +108,8 @@ class Progr(QMainWindow):
         self.points = []
 
     def drawfigure(self, qp):
+        qp.setBrush(self.colorbrush)
+        qp.setPen(self.colorpen)
         if self.polygondraw:
             qp.drawPolygon(QPolygon(self.points))
         if self.buttonellipseactiv and self.startpostaken:
@@ -139,6 +155,16 @@ class Progr(QMainWindow):
         if self.buttonlineactiv and self.startpostaken:
             point = self.points[0]
             qp.drawLine(point, QPoint(self.position[0], self.position[1]))
+        if self.buttoncurveactiv and self.startpostaken:
+            if self.buttoncurveactivfirst:
+                self.curvelines.append(QLine(self.points[0], QPoint(self.position[0], self.position[1])))
+                self.pointstart = QPoint(self.position[0], self.position[1])
+                self.buttoncurveactivfirst = False
+            else:
+                self.curvelines.append(QLine(self.pointstart, QPoint(self.position[0], self.position[1])))
+                self.pointstart = QPoint(self.position[0], self.position[1])
+            for _ in self.curvelines:
+                qp.drawLine(_)
 
 
 def except_hook(cls, exception, traceback):
