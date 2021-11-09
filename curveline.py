09fc76bbd5e6f3045c4b9642fbbab82ab8bdb4ac
AyazN <92933619+AyazN@users.noreply.github.com>
2021-11-09 18:48:32 +0300

    Add files via upload

---
+++ curveline.py
@@ -0,0 +1,9 @@
+class curvelines():
+    def __init__(self):
+        self.lines = []
+
+    def addline(self, line):
+        self.lines.append(line)
+
+    def alllines(self):
+        return self.lines
